c87403c69fe0d94b5a60bf4fd272743ca409cb41
Alexander Kozlov <alexabderl@coalla.ru>
2018-02-03 19:34:19 +0300

    fl_# footer +

--- src/views/components/Site_footer/Site_footer.js
+++ src/views/components/Site_footer/Site_footer.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import NextPin from '../../../img/next.svg';
+
 class Site_footer extends Component {
 
     render() {
         return (
 
             <footer className="site-footer" id="site-footer">
-                <div className="container">
-                    footer
+                <div className="site-footer__promo">
+                    <div className="container">
+                        <div className="site-footer__navigate">
+                            <a href="#" className="site-footer__next-link">
+                                <span>
+                                    Перейти на раздел <b>Струйных<span>&nbsp;</span>принтеров</b>
+                                </span>
+                            </a>
+
+                            <a href="#" className="button button--transparent button--next button--transparent-alternative">
+                                <img src={NextPin} alt=""/>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </footer>
 
