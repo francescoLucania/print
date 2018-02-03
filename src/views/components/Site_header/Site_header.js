17e4558437e291d1dbd607d2ca3dbfd806bdc523
Alexander Kozlov <alexabderl@coalla.ru>
2018-02-03 18:41:15 +0300

    fl_# header

--- src/views/components/Site_header/Site_header.js
+++ src/views/components/Site_header/Site_header.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
-class Site_header extends Component {
+import Logo from '../../../img/logo.svg';
+import MapPin from '../../../img/pin.svg';
 
+const siteName = 'brother';
+
+class Site_header extends Component {
 
     render() {
 
         return (
             <header className="site-header">
                 <div className="container">
-                    header
+                    <div className="site-header__grid">
+                        <a href="index.html" className="site-header__logo-box">
+                            <img src={Logo} alt={siteName}/>
+                        </a>
+                        <div className="site-header__button-box">
+                            <a href="#" className="button button--transparent button--map">
+                                <img src={MapPin} alt=""/>
+                                <span>Где купить?</span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </header>
         )
